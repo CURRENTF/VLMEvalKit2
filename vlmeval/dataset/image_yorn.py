2adb87c081a0caba77750e59cfcbd41924f6ebdd
jitaihao <1206013760@qq.com>
2025-09-18 17:30:43 +0800

    zh-ver

--- vlmeval/dataset/image_yorn.py
+++ vlmeval/dataset/image_yorn.py
@@ -9,8 +9,8 @@ class ImageYORNDataset(ImageBaseDataset):
     TYPE = 'Y/N'
 
     DATASET_URL = {
-        # 'POPE': '/root/LMUData/POPE_zh.tsv',
-        # 'MME': '/root/LMUData/MME_zh.tsv',
+        'POPE_zh': '/root/LMUData/POPE_zh.tsv',
+        'MME_zh': '/root/LMUData/MME_zh.tsv',
         'MME': 'https://opencompass.openxlab.space/utils/VLMEval/MME.tsv',
         'HallusionBench': 'https://opencompass.openxlab.space/utils/VLMEval/HallusionBench.tsv',
         'POPE': 'https://opencompass.openxlab.space/utils/VLMEval/POPE.tsv',
@@ -20,8 +20,8 @@ class ImageYORNDataset(ImageBaseDataset):
             "vsr_zeroshot_tsv/resolve/main/vsr_zeroshot_dataset_yn_strict.tsv"),}
 
     DATASET_MD5 = {
-        # 'POPE': '39240e8d0a80267cc9161e5a3e82f26c',
-        # 'MME': 'fc533077db37165935b1b958e35f3123',
+        'POPE_zh': '39240e8d0a80267cc9161e5a3e82f26c',
+        'MME_zh': 'fc533077db37165935b1b958e35f3123',
         'MME': 'b36b43c3f09801f5d368627fb92187c3',
         'POPE': 'c12f5acb142f2ef1f85a26ba2fbe41d5',
         'HallusionBench': '0c23ac0dc9ef46832d7a24504f2a0c7c',
